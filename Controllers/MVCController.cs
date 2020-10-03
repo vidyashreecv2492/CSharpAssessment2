78375c3a86f2e4833375cb61af8f8b737b808ebe
vidyashree c v <69243283+vidyashreecv2492@users.noreply.github.com>
2020-10-03 03:35:48 -0700

    Add files via upload

---
+++ Controllers/MVCController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExamWebApp.Models;
+
+namespace ExamWebApp.Controllers
+{
+    public class MVCController : Controller
+    {
+        public ViewResult Display()
+        {
+            var context = new MyDatabase();
+            var model = context.EmpTables.ToArray();
+            return View(model);
+        }
+        public ActionResult Find(string id)
+        {
+            int EmpId = int.Parse(id);
+            var context = new MyDatabase();
+            var model = context.EmpTables.FirstOrDefault((e) => e.EmpID == EmpId);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Find(EmpTable emp)
+        {
+            var context = new MyDatabase();
+            var model = context.EmpTables.FirstOrDefault((e) => e.EmpID == emp.EmpID);
+            model.EmpName = emp.EmpName;
+            model.EmpAddress = emp.EmpAddress;
+            model.EmpSalary = emp.EmpSalary;
+            context.SaveChanges();
+            return RedirectToAction("Display");
+        }
+        public ViewResult AddEmployee()
+        {
+            var model = new EmpTable();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(EmpTable emp)
+        {
+            var context = new MyDatabase();
+            context.EmpTables.Add(emp);
+            context.SaveChanges();
+            return RedirectToAction("Display");
+        }
+        public ActionResult DeleteEmployee(string id)
+        {
+            int EmpId = int.Parse(id);
+            var context = new MyDatabase();
+            var model = context.EmpTables.FirstOrDefault((e) => e.EmpID == EmpId);
+            context.EmpTables.Remove(model);
+            context.SaveChanges();
+            return RedirectToAction("Display");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamWebApp.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public double EmpSalary { get; set; }
+        public override string ToString()
+        {
+            return $"<p>Employee Id: {EmpID}</P><p>Name: {EmpName}</p><p>Address: {EmpAddress}</p><p>Salary: {EmpSalary}</p>";
+        }
+    }
+}
